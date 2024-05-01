3eb0deb4b76d3a6f691ea871a6a0da5377f82169
jimmy2683 <karangupta0805@gmail.com>
2024-05-01 08:08:40 +0530

    Changes In App-Constants and signup, login page

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,7 +1,7 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unique App ID, Region and Auth Key. 😊
 
 export const AppConstants = {
-  APP_ID: "256515ff3a562db8",
-  REGION: "IN",
-  AUTH_KEY: "81d5f65b39890b3acac5d34485ab965752519ced",
+  APP_ID: "id",
+  REGION: "region",
+  AUTH_KEY: "auth_key",
 };
